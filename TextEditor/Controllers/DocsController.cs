f758d37b5a236811e96172d72157257c8645a990
WalterMelon~ <vgberny@gmail.com~>
2024-12-01 12:09:54 -0600

    deply

--- TextEditor/Controllers/DocsController.cs
+++ TextEditor/Controllers/DocsController.cs
@@ -25,8 +25,11 @@ namespace TextEditor.Controllers
         // GET: Docs
         public async Task<IActionResult> Index()
         {
-            var applicationDbContext = _context.Docs.Include(d => d.User);
-            return View(await applicationDbContext.ToListAsync());
+            var applicationDbContext = from c in _context.Docs
+                                       select c;
+            applicationDbContext = applicationDbContext.Where(a => a.UserId == User.FindFirstValue(ClaimTypes.NameIdentifier));
+            
+            return View(await applicationDbContext.Include(d => d.User).ToListAsync());
         }
 
         // GET: Docs/Create
